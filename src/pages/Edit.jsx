11771d7c2f672e6f6787308611e0580e9d886fcd
Mingi Kim <161968750+Rune-of-GJ@users.noreply.github.com>
2025-06-17 12:25:03 +0900

    Update Edit.jsx
    sad

--- src/pages/Edit.jsx
+++ src/pages/Edit.jsx
@@ -1,7 +1,7 @@
 import { useParams, useNavigate } from "react-router-dom"
 import { useContext } from "react"
 import useDiary from "../hooks/useDiary"
-import Button from "../components/button"
+import Button from "../components/Button"
 import Header from "../components/Header"
 import Editor from "../components/Editor"
 import { DiaryDispatchContext } from "../App"
